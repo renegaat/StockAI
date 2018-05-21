f37dff7b21bdd236e766e967ea7b9e9779c6516c
joern <joernross@gmx.net>
2018-05-21 07:37:28 +0200

    stock ai initial commit

---
+++ application/app-common/src/main/java/de/pearlbay/stockai/common/AppCommonApplication.java
@@ -0,0 +1,12 @@
+package de.pearlbay.stockai.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCommonApplication.class, args);
+    }
+}
