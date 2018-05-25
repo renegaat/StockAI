4a69f171a558c59c97603fdca998597e7184a115
joern <joernross@gmx.net>
2018-05-25 07:34:50 +0200

    pom module and dependency setup

---
+++ application/app-stockrepo/src/main/java/de/pearlbay/stockai/stockrepo/StockrepoApplication.java
@@ -0,0 +1,12 @@
+package de.pearlbay.stockai.stockrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockrepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockrepoApplication.class, args);
+    }
+}
