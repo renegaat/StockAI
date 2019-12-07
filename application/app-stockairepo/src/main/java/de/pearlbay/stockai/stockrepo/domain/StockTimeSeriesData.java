8d6bfdb8136349ede676b457db11a5ae9e51208d
Joern Google <joern.ross@googlemail.com>
2019-12-07 20:42:07 +0100

    AI-02 Added Function enum. Added Alphavantage API Key to application.properties

--- application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/domain/StockTimeSeriesData.java
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/domain/StockTimeSeriesData.java
@@ -1,6 +1,7 @@
 package de.pearlbay.stockai.stockrepo.domain;
 
 import de.pearlbay.stockai.common.enums.Currency;
+import de.pearlbay.stockai.common.enums.Function;
 import de.pearlbay.stockai.common.enums.Market;
 import de.pearlbay.stockai.common.persistence.domain.BaseEntity;
 
@@ -13,4 +14,5 @@ public class StockTimeSeriesData extends BaseEntity {
     private boolean isNewData;
     private MetaData metaData;
     private List<TimeSeries> timeSeries;
+    private Function function;
 }
