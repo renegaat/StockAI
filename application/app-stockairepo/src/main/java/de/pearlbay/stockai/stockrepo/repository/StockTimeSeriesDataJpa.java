22ce7ea50c6c166a0f84ae3692f491b731e4047e
renegaat <joernross@gmx.net>
2020-01-06 21:26:28 +0100

    AI-03 jpa changes.

--- application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/StockTimeSeriesDataJpa.java
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/StockTimeSeriesDataJpa.java
@@ -1,10 +1,16 @@
 package de.pearlbay.stockai.stockrepo.repository;
 
+import de.pearlbay.stockai.common.enums.Currency;
+import de.pearlbay.stockai.common.enums.Function;
+import de.pearlbay.stockai.common.enums.Market;
 import de.pearlbay.stockai.common.persistence.repository.BaseJpa;
+import de.pearlbay.stockai.stockrepo.domain.MetaData;
+import de.pearlbay.stockai.stockrepo.domain.TimeSeries;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.List;
 
 /**
  *StockTimeSeriesDataJpa.
@@ -22,5 +28,19 @@ public class StockTimeSeriesDataJpa extends BaseJpa {
     @Column(name = "stocktimeseriesdata_id")
     private Long id;
 
+    private String stockName;
+    private Currency currency;
+    private Market market;
+    private boolean isNewData;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "metadata_id")
+    private MetaData metaData;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "metadata_id")
+    private List<TimeSeries> timeSeries;
+
+    private Function function;
 
 }
