1a04cf5eafbddd7fd6f2b67c82db876ab32b477b
renegaat <joernross@gmx.net>
2020-01-06 20:57:55 +0100

    AI-03 jpa changes.

--- application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/StockTimeSeriesDataJpa.java
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/StockTimeSeriesDataJpa.java
@@ -1,10 +1,26 @@
 package de.pearlbay.stockai.stockrepo.repository;
 
 import de.pearlbay.stockai.common.persistence.repository.BaseJpa;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
 
 /**
  *StockTimeSeriesDataJpa.
  *@author joern ross (pearlbay) 2020 
  */
+@Entity
+@Table(name = "stocktimeseriesdata")
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class StockTimeSeriesDataJpa extends BaseJpa {
+
+    @Id
+    @GeneratedValue(generator = "stock_timeseries_data_id_seq")
+    @SequenceGenerator(name = "stock_timeseries_data_id_seq", sequenceName = "stock_timeseries_data_id_seq")
+    @Column(name = "stocktimeseriesdata_id")
+    private Long id;
+
+
 }
