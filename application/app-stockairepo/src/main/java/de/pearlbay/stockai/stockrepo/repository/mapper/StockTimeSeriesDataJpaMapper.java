058ba5cbab8692a1a7a34140177bacb6e6a6fafd
renegaat <joernross@gmx.net>
2020-01-12 22:55:57 +0100

    AI-14 Fixed checkstyle errors.

--- application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/mapper/StockTimeSeriesDataJpaMapper.java
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/mapper/StockTimeSeriesDataJpaMapper.java
@@ -3,17 +3,14 @@ package de.pearlbay.stockai.stockrepo.repository.mapper;
 
 import de.pearlbay.stockai.common.mapper.PriceMapper;
 import de.pearlbay.stockai.stockrepo.domain.StockTimeSeriesData;
-import de.pearlbay.stockai.stockrepo.domain.TimeSeries;
 import de.pearlbay.stockai.stockrepo.repository.StockTimeSeriesDataJpa;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 /**
  * StockTimeSeriesDataJpaMapper.
  * @author joern ross (pearlbay) 2020
  */
-@Mapper(uses= {PriceMapper.class, TimeSeriesJpaMapper.class, MetaDataJpaMapper.class})
+@Mapper(uses = {PriceMapper.class, TimeSeriesJpaMapper.class, MetaDataJpaMapper.class})
 public interface StockTimeSeriesDataJpaMapper {
     StockTimeSeriesDataJpa toJpa(StockTimeSeriesData s);
     StockTimeSeriesData fromJpa(StockTimeSeriesDataJpa jpa);
