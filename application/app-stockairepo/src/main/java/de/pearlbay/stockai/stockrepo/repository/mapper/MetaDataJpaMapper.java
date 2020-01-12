21cca73481cacc4dca95d4ab27228908cbf71202
renegaat <joernross@gmx.net>
2020-01-12 22:35:22 +0100

    AI-14 MetaDataMapper added.

--- application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/mapper/MetaDataJpaMapper.java
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/mapper/MetaDataJpaMapper.java
@@ -1,7 +1,16 @@
 package de.pearlbay.stockai.stockrepo.repository.mapper;
+
+import de.pearlbay.stockai.stockrepo.domain.MetaData;
+import de.pearlbay.stockai.stockrepo.repository.MetaDataJpa;
+import org.mapstruct.Mapper;
+
+
 /**
  * MetaDataJpaMapper.
  * @author joern ross (pearlbay) 2020
  */
+@Mapper
 public interface MetaDataJpaMapper {
+    MetaDataJpa toJpa(MetaData m);
+    MetaData fromJpa(MetaDataJpa jpa);
 }
