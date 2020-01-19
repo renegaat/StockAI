9666c70661484bb754385e36fd9ca50d49643b31
renegaat <joernross@gmx.net>
2020-01-19 16:49:07 +0100

    AI-15 Added TimeSeriesJpaMapper Test.

--- application/app-stockairepo/src/test/java/de/pearlbay/stockai/stockrepo/repository/MapperTest.java
+++ application/app-stockairepo/src/test/java/de/pearlbay/stockai/stockrepo/repository/MapperTest.java
@@ -1,8 +1,11 @@
 package de.pearlbay.stockai.stockrepo.repository;
 
 
+import de.pearlbay.stockai.common.domain.Price;
 import de.pearlbay.stockai.stockrepo.domain.MetaData;
+import de.pearlbay.stockai.stockrepo.domain.TimeSeries;
 import de.pearlbay.stockai.stockrepo.repository.mapper.MetaDataJpaMapperImpl;
+import de.pearlbay.stockai.stockrepo.repository.mapper.TimeSeriesJpaMapperImpl;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -11,14 +14,14 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 
-
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 public class MapperTest {
 
     @Test
-    public void shouldMapMetaDataToJpaAndBack(){
+    public void shouldMapMetaDataToJpaAndBack() {
 
         MetaDataJpaMapperImpl metaDataJpaMapper = new MetaDataJpaMapperImpl();
 
@@ -29,5 +32,22 @@ public class MapperTest {
         MetaData result = metaDataJpaMapper.fromJpa(metaDataJpa);
 
         assertEquals(metaData, result);
+    }
+
+    @Test
+    public void shouldMapTimeSeriesToJpaAndBack() {
+
+        TimeSeriesJpaMapperImpl timeSeriesJpaMapper = new TimeSeriesJpaMapperImpl();
+
+        Price testPrice = new Price();
+        testPrice.setAmount(BigDecimal.ONE);
+
+        TimeSeries timeSeries = TimeSeries.builder().time(LocalDateTime.now()).close(testPrice)
+                .high(testPrice).low(testPrice).open(testPrice).volume(100).build();
+
+        TimeSeriesJpa timeSeriesJpa = timeSeriesJpaMapper.toJpa(timeSeries);
+        TimeSeries result = timeSeriesJpaMapper.fromJpa(timeSeriesJpa);
+
+        assertEquals(timeSeries, result);
     }
 }
