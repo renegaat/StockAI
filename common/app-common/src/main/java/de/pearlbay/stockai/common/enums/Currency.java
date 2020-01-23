b993cdddd47a0bcaa324bf337059e534ee945dfc
renegaat <joernross@gmx.net>
2020-01-23 04:42:52 +0100

    AI-15 Started filling currency enums.

--- common/app-common/src/main/java/de/pearlbay/stockai/common/enums/Currency.java
+++ common/app-common/src/main/java/de/pearlbay/stockai/common/enums/Currency.java
@@ -4,4 +4,29 @@ package de.pearlbay.stockai.common.enums;
  * @author joern ross (pearlbay) 2020
  */
 public enum Currency {
+    AED,
+    AFN,
+    ALL,
+    AMD,
+    ANG,
+    AOA,
+    ARS,
+    AUD,
+    AWG,
+    AZN,
+    BAM,
+    BBD,
+    BDT,
+    BGN,
+    BHD,
+    BIF,
+    BMD,
+    BND,
+    BOB,
+    BRL,
+    BSD,
+    BTN,
+    BWP,
+    BZD
+
 }
