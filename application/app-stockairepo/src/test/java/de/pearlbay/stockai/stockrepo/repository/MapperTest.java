9f7db763c936867a9eeb5b1b434f5609c795b32d
renegaat <joernross@gmx.net>
2020-01-24 06:36:50 +0100

    AI-15 Added StockTimeSeriesDataMapperTest.

--- application/app-stockairepo/src/test/java/de/pearlbay/stockai/stockrepo/repository/MapperTest.java
+++ application/app-stockairepo/src/test/java/de/pearlbay/stockai/stockrepo/repository/MapperTest.java
@@ -2,9 +2,15 @@ package de.pearlbay.stockai.stockrepo.repository;
 
 
 import de.pearlbay.stockai.common.domain.Price;
+import de.pearlbay.stockai.common.enums.Currency;
+import de.pearlbay.stockai.common.enums.Function;
+import de.pearlbay.stockai.common.enums.Market;
 import de.pearlbay.stockai.stockrepo.domain.MetaData;
+import de.pearlbay.stockai.stockrepo.domain.StockTimeSeriesData;
 import de.pearlbay.stockai.stockrepo.domain.TimeSeries;
 import de.pearlbay.stockai.stockrepo.repository.mapper.MetaDataJpaMapperImpl;
+import de.pearlbay.stockai.stockrepo.repository.mapper.StockTimeSeriesDataJpaMapper;
+import de.pearlbay.stockai.stockrepo.repository.mapper.StockTimeSeriesDataJpaMapperImpl;
 import de.pearlbay.stockai.stockrepo.repository.mapper.TimeSeriesJpaMapperImpl;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -55,7 +61,27 @@ public class MapperTest {
         assertEquals(timeSeries, result);
     }
 
-    //todo rossj add stocktimeseriesdata test.
+    @Test
+    public void shouldMapStocklTimeSeriesDataToJpaAndBack(){
+
+        StockTimeSeriesDataJpaMapperImpl stockTimeSeriesDataJpaMapper = new StockTimeSeriesDataJpaMapperImpl();
+
+        MetaData metaData = MetaData.builder().information("Information").lastRefreshed(LocalDateTime.now())
+                .symbol("Symbol").timeZone(ZoneId.systemDefault()).build();
+
+        StockTimeSeriesData stockTimeSeriesData = StockTimeSeriesData.builder().metaData(metaData)
+                .newData(true)
+                .timeSeries(retrieveTimeSeriesList()).stockName("Stockname").currency(Currency.BRL)
+                .function(Function.TIME_SERIES_DAILY)
+                .market(Market.CAD)
+                .build();
+
+        StockTimeSeriesDataJpa stockTimeSeriesDataJpa = stockTimeSeriesDataJpaMapper.toJpa(stockTimeSeriesData);
+        StockTimeSeriesData result = stockTimeSeriesDataJpaMapper.fromJpa(stockTimeSeriesDataJpa);
+
+        assertEquals(stockTimeSeriesData, result);
+    }
+
 
     private List retrieveTimeSeriesList() {
 
