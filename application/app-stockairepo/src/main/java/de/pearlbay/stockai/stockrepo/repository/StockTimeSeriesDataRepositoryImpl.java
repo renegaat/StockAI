6f9b39999afb441226bea67bdcf7f3a4461f2f46
Joern Google <joern.ross@googlemail.com>
2020-02-01 18:30:18 +0100

    AI-14 minor changes to repository.

--- application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/StockTimeSeriesDataRepositoryImpl.java
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/StockTimeSeriesDataRepositoryImpl.java
@@ -1,11 +1,15 @@
 package de.pearlbay.stockai.stockrepo.repository;
 
 import de.pearlbay.stockai.stockrepo.domain.repository.StockTimeSeriesDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 /**
- * StockTimeSeriesDataJpaRepository.
+ * StockTimeSeriesDataRepository.
  * @author joern ross (pearlbay) 2020
  */
-
+@Repository
 public class StockTimeSeriesDataRepositoryImpl implements StockTimeSeriesDataRepository {
+    @Autowired
+    private StockTimeSeriesDataJpaRepository stockTimeSeriesDataJpaRepository;
 }
