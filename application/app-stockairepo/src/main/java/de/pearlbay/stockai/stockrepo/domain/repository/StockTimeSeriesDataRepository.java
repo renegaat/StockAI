ff53529e4e7c2927a9e982d9196acf8d5b40ff93
Joern Google <joern.ross@googlemail.com>
2020-02-01 19:06:23 +0100

    AI-14 added crud methods to repository.

--- application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/domain/repository/StockTimeSeriesDataRepository.java
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/domain/repository/StockTimeSeriesDataRepository.java
@@ -1,8 +1,17 @@
 package de.pearlbay.stockai.stockrepo.domain.repository;
 
+import de.pearlbay.stockai.common.enums.Function;
+import de.pearlbay.stockai.stockrepo.repository.StockTimeSeriesDataJpa;
+
+import java.time.LocalDateTime;
+
 /**
  * StockTimeSeriesDataRepository.
  * @author joern ross (pearlbay) 2020
  */
 public interface StockTimeSeriesDataRepository {
+    StockTimeSeriesDataJpa save(StockTimeSeriesDataJpa stockTimeSeriesDataJpa);
+    StockTimeSeriesDataJpa findBySymbolAndFunctionAndDate(String symbol, Function function, LocalDateTime begin,
+                                                          LocalDateTime end);
+    StockTimeSeriesDataJpa findBySymbolAndFunction(String symbol, Function function);
 }
--- application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/StockTimeSeriesDataRepositoryImpl.java
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/StockTimeSeriesDataRepositoryImpl.java
@@ -1,8 +1,12 @@
 package de.pearlbay.stockai.stockrepo.repository;
 
+import de.pearlbay.stockai.common.enums.Function;
 import de.pearlbay.stockai.stockrepo.domain.repository.StockTimeSeriesDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 /**
  * StockTimeSeriesDataRepository.
@@ -12,4 +16,19 @@ import org.springframework.stereotype.Repository;
 public class StockTimeSeriesDataRepositoryImpl implements StockTimeSeriesDataRepository {
     @Autowired
     private StockTimeSeriesDataJpaRepository stockTimeSeriesDataJpaRepository;
+    @Override
+    public StockTimeSeriesDataJpa save(StockTimeSeriesDataJpa stockTimeSeriesDataJpa) {
+        return stockTimeSeriesDataJpaRepository.save(stockTimeSeriesDataJpa);
+    }
+    @Query
+    @Override
+    public StockTimeSeriesDataJpa findBySymbolAndFunctionAndDate(String symbol, Function function,
+                                                                 LocalDateTime begin, LocalDateTime end) {
+        return null;
+    }
+    @Query
+    @Override
+    public StockTimeSeriesDataJpa findBySymbolAndFunction(String symbol, Function function) {
+        return null;
+    }
 }
