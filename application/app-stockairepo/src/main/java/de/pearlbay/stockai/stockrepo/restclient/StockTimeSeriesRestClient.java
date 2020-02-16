9baca5be90cf6fa5a6e87858d53b147bdb31579b
renegaat <joernross@gmx.net>
2020-02-16 08:51:45 +0100

    AI-09 added StockTimeSeriesRestClient.

---
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/restclient/StockTimeSeriesRestClient.java
@@ -0,0 +1,10 @@
+package de.pearlbay.stockai.stockrepo.restclient;
+
+
+/**
+ * StockTimeSeriesRestClient
+ * Retrieves stockdata json from service.
+ * @author joern ross (pearlbay) 2020
+ */
+public interface StockTimeSeriesRestClient {
+}
---
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/restclient/StockTimeSeriesRestClientImpl.java
@@ -0,0 +1,11 @@
+package de.pearlbay.stockai.stockrepo.restclient;
+
+import org.springframework.stereotype.Component;
+/**
+ * StockTimeSeriesRestClient
+ * Retrieves stockdata json from service.
+ * @author joern ross (pearlbay) 2020
+ */
+@Component
+public class StockTimeSeriesRestClientImpl implements StockTimeSeriesRestClient {
+}
