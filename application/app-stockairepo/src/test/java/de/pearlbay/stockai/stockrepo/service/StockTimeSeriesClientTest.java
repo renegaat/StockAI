b0c25ca25639d244438a4ee8ff34fe9eee0d9181
renegaat <joernross@gmx.net>
2020-02-17 09:28:17 +0100

    AI-09 added StockTimeSeriesClientTest stub.

---
+++ application/app-stockairepo/src/test/java/de/pearlbay/stockai/stockrepo/service/StockTimeSeriesClientTest.java
@@ -0,0 +1,9 @@
+package de.pearlbay.stockai.stockrepo.service;
+
+import org.junit.Test;
+
+public class StockTimeSeriesClientTest {
+    @Test
+    public void testRetrieveStockTimeSeriesData() {
+    }
+}
