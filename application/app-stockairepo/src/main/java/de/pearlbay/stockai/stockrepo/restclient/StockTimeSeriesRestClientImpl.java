1d3f27c69d04526d303e3d4f1bd7f9256cfaa15d
renegaat <joernross@gmx.net>
2020-02-22 03:03:09 +0100

    AI-09 StockTimeSeriesRestClientImpl development.

--- application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/restclient/StockTimeSeriesRestClientImpl.java
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/restclient/StockTimeSeriesRestClientImpl.java
@@ -2,9 +2,11 @@ package de.pearlbay.stockai.stockrepo.restclient;
 
 import de.pearlbay.stockai.common.enums.Function;
 import de.pearlbay.stockai.common.enums.OutputSize;
+import de.pearlbay.stockai.stockrepo.restclient.dto.MetaDataDto;
 import de.pearlbay.stockai.stockrepo.restclient.dto.StockTimeSeriesDataDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * StockTimeSeriesRestClient
@@ -21,6 +23,16 @@ public class StockTimeSeriesRestClientImpl implements StockTimeSeriesRestClient 
     @Override
     public StockTimeSeriesDataDto retrieveStockTimeSeriesDataDto(String apiKey, String symbol,
                                                                  Function function, OutputSize outputSize) {
-        return null;
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url  = apiUrl
+                + "function=" + function.name()
+                + "&symbol=" + symbol
+                + "&apikey=" + apiKey
+                + "&outputsize=" + outputSize;
+
+        MetaDataDto metaDataDto = restTemplate.getForObject(url, MetaDataDto.class);
+        return  null;
     }
 }
