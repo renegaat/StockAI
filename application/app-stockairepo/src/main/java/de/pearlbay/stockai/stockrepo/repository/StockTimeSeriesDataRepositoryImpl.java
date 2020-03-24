0001543f8966c1f5a451a4fcd3265f4e05c9320a
Joern Google <joern.ross@googlemail.com>
2020-03-24 06:12:57 +0100

    AI-09 Added transactional annotation to repository.

--- application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/StockTimeSeriesDataRepositoryImpl.java
+++ application/app-stockairepo/src/main/java/de/pearlbay/stockai/stockrepo/repository/StockTimeSeriesDataRepositoryImpl.java
@@ -3,7 +3,8 @@ package de.pearlbay.stockai.stockrepo.repository;
 import de.pearlbay.stockai.common.enums.Function;
 import de.pearlbay.stockai.stockrepo.domain.repository.StockTimeSeriesDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * StockTimeSeriesDataRepository.
@@ -11,13 +12,15 @@ import org.springframework.stereotype.Service;
  * @author joern ross (pearlbay) 2020
  */
 
-@Service
+@Repository
+@Transactional
 public class StockTimeSeriesDataRepositoryImpl implements StockTimeSeriesDataRepository {
     @Autowired
     private StockTimeSeriesDataJpaRepository stockTimeSeriesDataJpaRepository;
+
     @Override
     public StockTimeSeriesDataJpa save(StockTimeSeriesDataJpa stockTimeSeriesDataJpa) {
-        return stockTimeSeriesDataJpaRepository.save(stockTimeSeriesDataJpa);
+        return stockTimeSeriesDataJpaRepository.saveAndFlush(stockTimeSeriesDataJpa);
     }
 
     @Override
