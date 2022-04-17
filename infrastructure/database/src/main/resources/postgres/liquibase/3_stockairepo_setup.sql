2da4f6373beac1d0089ffee87d9dc7cd3bc1b490
jornross <joernrossgmx.net>
2022-04-17 11:40:25 +0200

    AI-24 postgres integration

--- infrastructure/database/src/main/resources/postgres/liquibase/3_stockairepo_setup.sql
+++ infrastructure/database/src/main/resources/postgres/liquibase/3_stockairepo_setup.sql
@@ -1,11 +1,2 @@
-call drop_user_if_exists ('stockairepo');
 
 CREATE USER stockairepo
-  IDENTIFIED BY "stockai"
-  DEFAULT TABLESPACE stockairepo
-  TEMPORARY TABLESPACE temp
-  QUOTA UNLIMITED ON stockairepo;
-
-GRANT CONNECT TO stockairepo;
-
-GRANT RESOURCE TO stockairepo;
